992194a7154eb28867c0af0c19df909fa6cbdcc5
Asya Lem <anundam@gmail.com>
2018-10-12 16:48:15 +0100

    added profile template, setup mongoose, connected to mongoDB, added config file

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const path = require('path')
+const keys = require('./config/keys');
 
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, {useNewUrlParser: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('successfully connected to DB')
+});
+
+const app = express();
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', async (req, res) => {
+app.get('/', async(req, res) => {
     res.render('home.pug')
 })
 
-
+app.get('/profile', async(req, res) => {
+    res.render('profile.pug')
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Listening on port`, PORT);
-  });
-  
+});
